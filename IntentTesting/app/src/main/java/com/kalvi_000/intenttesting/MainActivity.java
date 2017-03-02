b171821d5059da638dcf3e9f923e969d5c6d88c2
kalvin66rocks <kalvin66rocks@gmail.com>
2017-03-02 11:16:13 -0500

    In class work on passing data between different intetnts

--- IntentTesting/app/src/main/java/com/kalvi_000/intenttesting/MainActivity.java
+++ IntentTesting/app/src/main/java/com/kalvi_000/intenttesting/MainActivity.java
@@ -1,13 +1,33 @@
 package com.kalvi_000.intenttesting;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.support.v7.appcompat.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final int REQUEST_CODE = 10;
+    int mainNum=5;
+    TextView mainTextView;
+    Intent callSecondary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainTextView = (TextView) findViewById(R.id.textView);
+        callSecondary = new Intent(this, SecondaryActivity.class);
+    }
+
+    public void OnClickCallSecond(View v){
+        Log.d("Brenneman","Entering OnClickCallSecond");
+        callSecondary.putExtra("Value from Main",mainNum);
+        startActivityForResult(callSecondary,REQUEST_CODE);
     }
 }
