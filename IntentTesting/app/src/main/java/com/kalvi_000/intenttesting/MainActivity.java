9129f662ef298491a426b52b0f8ad5f290679699
kalvin66rocks <kalvin66rocks@gmail.com>
2017-03-14 10:55:20 -0400

    Some in class work, mainly focusing on intent test, passing data between two different activities

--- IntentTesting/app/src/main/java/com/kalvi_000/intenttesting/MainActivity.java
+++ IntentTesting/app/src/main/java/com/kalvi_000/intenttesting/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kalvi_000.intenttesting;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,7 +21,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null){
+            Log.d("Brenneman","saved instance");
+            mainNum=savedInstanceState.getInt("CurrentNumber");
+        }
         setContentView(R.layout.activity_main);
+
         mainTextView = (TextView) findViewById(R.id.textView);
         callSecondary = new Intent(this, SecondaryActivity.class);
     }
@@ -29,5 +35,26 @@ public class MainActivity extends AppCompatActivity {
         Log.d("Brenneman","Entering OnClickCallSecond");
         callSecondary.putExtra("Value from Main",mainNum);
         startActivityForResult(callSecondary,REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("Brenneman","Entering onActivityResult");
+        if(requestCode==REQUEST_CODE){
+            if(resultCode== Activity.RESULT_OK){
+                mainNum= data.getIntExtra("The Changed Number",0);
+                mainTextView.setText(Integer.toString(mainNum));
+            }
+            else{
+                Log.d("Brenneman","Something cancelled the second call");
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("CurrentNumber",mainNum);
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
