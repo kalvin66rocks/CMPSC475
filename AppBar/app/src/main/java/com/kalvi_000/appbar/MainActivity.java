10574e241e9e5d53d9412883252f1f4d1cc6bc8a
kalvin66rocks <kalvin66rocks@gmail.com>
2017-03-16 16:22:43 -0400

    showed title bar and how to do action button and action bar things

--- AppBar/app/src/main/java/com/kalvi_000/appbar/MainActivity.java
+++ AppBar/app/src/main/java/com/kalvi_000/appbar/MainActivity.java
@@ -2,6 +2,9 @@ package com.kalvi_000.appbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_settings2:
+                Toast.makeText(this, "action Settings 2", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings3:
+                Toast.makeText(this, "action Settings 3", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings4:
+                Toast.makeText(this, "action Settings 4", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings5:
+                Toast.makeText(this, "action Settings 5", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
