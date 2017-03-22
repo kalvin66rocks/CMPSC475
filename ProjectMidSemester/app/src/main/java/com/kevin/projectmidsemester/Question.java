5d13a3bd11735283b03c79358c36a6e0258201d4
kalvin66rocks <kalvin66rocks@gmail.com>
2017-03-21 22:35:45 -0400

    Added the options to be picked as well as the options in the Action bar.
    Still have to add a new activity and layout to be able to show the results of people answering that questions as well as sharing information between these intents to record tries

--- ProjectMidSemester/app/src/main/java/com/kevin/projectmidsemester/Question.java
+++ ProjectMidSemester/app/src/main/java/com/kevin/projectmidsemester/Question.java
@@ -1,10 +1,14 @@
 package com.kevin.projectmidsemester;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /**
  * Created by Kevin on 3/15/2017.
@@ -21,6 +25,36 @@ public class Question extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.question_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.layoutSSS);
+        switch (id){
+            case R.id.action_settings2:
+                Toast.makeText(this, "Cyan", Toast.LENGTH_SHORT).show();
+                layout.setBackgroundColor(Color.CYAN);
+                return true;
+            case R.id.action_settings3:
+                Toast.makeText(this, "Green", Toast.LENGTH_SHORT).show();
+                layout.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.action_settings4:
+                Toast.makeText(this, "Red", Toast.LENGTH_SHORT).show();
+                layout.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.action_settings7:
+                Toast.makeText(this, "2 tries, tries reset", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings8:
+                Toast.makeText(this, "3 tries, tries reset", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings9:
+                Toast.makeText(this, "4 tries, tries reset", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void goToMain(View view){
