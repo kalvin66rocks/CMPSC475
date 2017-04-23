b0b1a190994293a9ecaf6758078e8840ea2240f5
kalvin66rocks <kalvin66rocks@gmail.com>
2017-04-22 22:47:46 -0400

    added crucial line in gradle to allow android app to incorporate google services: compile 'com.google.android.gms:play-services:10.2.1' "makes the world go round" -- much more work to come also created listener service

---
+++ FinalProject/app/src/main/java/com/kalvi_000/finalproject/WearListenerService.java
@@ -0,0 +1,12 @@
+package com.kalvi_000.finalproject;
+
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * Created by Kevin on 4/22/2017.
+ */
+
+public class WearListenerService extends WearableListenerService {
+
+
+}
