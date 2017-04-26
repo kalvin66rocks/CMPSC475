ce3ad6bcb8dafec1d9b516c80c744aeb8de0b91c
kalvin66rocks <kalvin66rocks@gmail.com>
2017-04-26 19:12:25 -0400

    some final changes to the attempt with the wear app

---
+++ FinalProject/app/src/main/java/com/kalvi_000/finalproject/AlternativeListener.java
@@ -0,0 +1,33 @@
+package com.kalvi_000.finalproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Kevin on 4/26/2017.
+ */
+
+public class AlternativeListener extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        // Get the intent that started this activity
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+
+        if (intent.getType().equals("text/plain")) {
+            String extra = intent.getStringExtra(Intent.EXTRA_TEXT);
+            Log.d("OnMessageRevieved *****", extra);
+            Toast.makeText(this, "We recieved text!" ,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
