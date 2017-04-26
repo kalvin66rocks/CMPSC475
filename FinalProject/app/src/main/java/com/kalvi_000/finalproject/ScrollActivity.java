46e8067d36aeecf6f362ca0a459e617afcea5618
kalvin66rocks <kalvin66rocks@gmail.com>
2017-04-25 23:26:53 -0400

    worked on scroll view, added images to vcs

--- FinalProject/app/src/main/java/com/kalvi_000/finalproject/ScrollActivity.java
+++ FinalProject/app/src/main/java/com/kalvi_000/finalproject/ScrollActivity.java
@@ -1,13 +1,23 @@
 package com.kalvi_000.finalproject;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 /**
  * Created by Kevin on 4/23/2017.
  */
 
 public class ScrollActivity extends Activity {
+
+    ImageView card1;
+    ImageView card2;
+    ImageView card3;
+    ImageView card4;
+    ImageView card5;
+
+    Drawable draw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,21 @@ public class ScrollActivity extends Activity {
     }
 
     private void connectViews(){
+        //because images are stupid, do these steps to make images properly viewable
+        card1 = (ImageView) findViewById(R.id.imageView);
+        card1.setImageResource(0);
+        draw = getResources().getDrawable(R.drawable.brainstorm);
+        card1.setImageDrawable(draw);
+
+        card2 = (ImageView) findViewById(R.id.imageView2);
+        card2.setImageResource(0);
+        draw = getResources().getDrawable(R.drawable.ancestral_recall);
+        card2.setImageDrawable(draw);
+
+        card3 = (ImageView) findViewById(R.id.imageView3);
+        card3.setImageResource(0);
+        draw = getResources().getDrawable(R.drawable.survivalofthefittest);
+        card3.setImageDrawable(draw);
 
     }
 
